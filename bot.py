f8d58b4eb32abfa803fbcb4719c3ce217d7e4ff9
Kos03134 <55111046+Kos03134@users.noreply.github.com>
2019-09-10 17:04:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,14 @@ bot = telebot.TeleBot('968928190:AAE8iCHSU63HAcommSlsoVg_5J0OP2gNfw8')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'you`ve entered command /start')
 
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == '':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
 bot.polling()
